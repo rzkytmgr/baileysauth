87b34ea4fbea6186e0dabc291296f363f5c85de2
rzkytmgr <rzkytmgr@gmail.com>
2025-08-09 07:33:03 +0700

    🔧 chore(lint): update eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,16 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config({
-  extends: [
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-  ],
-  rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
-  },
-  "ignores": [
-    "node_modules",
-    "dist/**/*",
-  ],
+    extends: [
+        eslint.configs.recommended,
+        tseslint.configs.recommended,
+    ],
+    rules: {
+        "@typescript-eslint/no-unused-vars": "warn",
+        "@typescript-eslint/no-explicit-any": "off",
+    },
+    "ignores": [
+        "node_modules",
+        "dist/**/*",
+    ],
 });
