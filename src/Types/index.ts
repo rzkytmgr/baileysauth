fe059b4f0c0b3fad68804f5acb0c2851f9cd122b
rzkytmgr <rzkytmgr@gmail.com>
2026-07-04 16:57:59 +0700

    refactor(types): import all required types from baileys types

--- src/Types/index.ts
+++ src/Types/index.ts
@@ -12,6 +12,12 @@ import type {
     ConnectionConfig,
     Client as PostgeSQLClient,
 } from "pg";
+import type {
+    AuthenticationCreds,
+    AuthenticationState,
+    SignalDataTypeMap,
+    KeyPair,
+} from "baileys/lib/Types/Auth";
 
 export type SessionDBResult = {
     name: string;
@@ -117,146 +123,15 @@ export type AppDataSync = {
     timestamp: Long | number;
 };
 
-/** Baileys Types */
-export type AuthenticationState = {
-    creds: AuthenticationCreds;
-    keys: SignalKeyStore;
-};
-
-export type SignalCreds = {
-    readonly signedIdentityKey: KeyPair;
-    readonly signedPreKey: SignedKeyPair;
-    readonly registrationId: number;
-};
-
-export type KeyPair = {
-    public: Uint8Array;
-    private: Uint8Array;
-};
-
-export type SignedKeyPair = {
-    keyPair: KeyPair;
-    signature: Uint8Array;
-    keyId: number;
-    timestampS?: number;
-};
-
-export type ProtocolAddress = {
-    name: string;
-    deviceId: number;
-};
-
-export type SignalIdentity = {
-    identifier: ProtocolAddress;
-    identifierKey: Uint8Array;
-};
-
-export type IMeDetails = {
-    id: string;
-    lid?: string;
-    phoneNumber?: string;
-    name?: string;
-    notify?: string;
-    verifiedName?: string;
-    imgUrl?: string | null;
-    status?: string;
-};
-
-export type IAccount = {
-    details?: Uint8Array | null;
-    accountSignatureKey?: Uint8Array | null;
-    accountSignature?: Uint8Array | null;
-    deviceSignature?: Uint8Array | null;
-};
-
-export type IMessageKey = {
-    remoteJid?: string | null;
-    fromMe?: boolean | null;
-    id?: string | null;
-    participant?: string | null;
-};
-
-export type MinimalMessage = {
-    key: IMessageKey;
-    messageTimestamp?: number | Long | null;
-};
-
-export type AccountSettings = {
-    unarchiveChats: boolean;
-    defaultDisappearingMode?: IConversation;
-};
-
-export type IConversation = {
-    ephemeralExpiration?: number | null;
-    ephemeralSettingTimestamp?: number | Long | null;
-};
-
-export type AuthenticationCreds = SignalCreds & {
-    readonly noiseKey: KeyPair;
-    readonly pairingEphemeralKeyPair: KeyPair;
-    advSecretKey: string;
-    me?: IMeDetails;
-    account?: IAccount;
-    signalIdentities?: SignalIdentity[];
-    myAppStateKeyId?: string;
-    firstUnuploadedPreKeyId: number;
-    nextPreKeyId: number;
-    lastAccountSyncTimestamp?: number;
-    platform?: string;
-    processedHistoryMessages: MinimalMessage[];
-    accountSyncCounter: number;
-    accountSettings: AccountSettings;
-    registered: boolean;
-    pairingCode: string | undefined;
-    lastPropHash: string | undefined;
-    routingInfo: Buffer | undefined;
-    additionalData?: any | undefined;
-};
-
-type Awaitable<T> = T | Promise<T>;
-
-export type SignalKeyStore = {
-    get<T extends keyof SignalDataTypeMap>(type: T, ids: string[]): Awaitable<{ [id: string]: SignalDataTypeMap[T]; }>;
-    set(data: SignalDataSet): Awaitable<void>;
-    clear?(): Awaitable<void>;
-};
-
-export type SignalDataSet = { [T in keyof SignalDataTypeMap]?: { [id: string]: SignalDataTypeMap[T] | null; }; };
-
-export type SignalDataTypeMap = {
-    session: Uint8Array;
-    "pre-key": KeyPair;
-    "sender-key": Uint8Array;
-    "sender-key-memory": { [jid: string]: boolean; };
-    "app-state-sync-key": IAppStateSyncKey;
-    "app-state-sync-version": LTHashState;
-    "lid-mapping": string;
-    "device-list": string[];
-};
-
-export type IAppStateSyncKey = {
-    keyData?: Uint8Array | null;
-    fingerprint?: IFingerprintState | null;
-    timestamp?: number | Long | null;
-};
-
-export type IFingerprintState = {
-    rawId?: number | null;
-    currentIndex?: number | null;
-    deviceIndexes?: number[] | null;
-};
-
-export type LTHashState = {
-    version: number;
-    hash: Buffer;
-    indexValueMap: {
-        [indexMacBase64: string]: { valueMac: Uint8Array | Buffer; };
-    };
-};
-
 export type BaileysAuthState = {
     state: AuthenticationState;
     saveCreds: () => Promise<any>;
     wipeCreds: () => Promise<any>;
     close: () => Promise<void>;
+};
+
+export type {
+    AuthenticationCreds,
+    SignalDataTypeMap,
+    KeyPair,
 };
